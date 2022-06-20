590cc9fd59de761ad3fd1e779db2604911c71f7e
samjesus8 <98812930+samjesus8@users.noreply.github.com>
2022-06-21 00:27:32 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SimpleCalculatorProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.B0 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B4 = new System.Windows.Forms.Button();
+            this.B5 = new System.Windows.Forms.Button();
+            this.B6 = new System.Windows.Forms.Button();
+            this.B7 = new System.Windows.Forms.Button();
+            this.B8 = new System.Windows.Forms.Button();
+            this.B9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 12);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(346, 72);
+            this.outputBox.TabIndex = 0;
+            // 
+            // B0
+            // 
+            this.B0.Location = new System.Drawing.Point(13, 366);
+            this.B0.Name = "B0";
+            this.B0.Size = new System.Drawing.Size(75, 61);
+            this.B0.TabIndex = 1;
+            this.B0.Text = "0";
+            this.B0.UseVisualStyleBackColor = true;
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(13, 299);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(75, 61);
+            this.B1.TabIndex = 2;
+            this.B1.Text = "1";
+            this.B1.UseVisualStyleBackColor = true;
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(94, 299);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(75, 61);
+            this.B2.TabIndex = 3;
+            this.B2.Text = "2";
+            this.B2.UseVisualStyleBackColor = true;
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(175, 299);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(75, 61);
+            this.B3.TabIndex = 4;
+            this.B3.Text = "3";
+            this.B3.UseVisualStyleBackColor = true;
+            // 
+            // B4
+            // 
+            this.B4.Location = new System.Drawing.Point(12, 232);
+            this.B4.Name = "B4";
+            this.B4.Size = new System.Drawing.Size(75, 61);
+            this.B4.TabIndex = 5;
+            this.B4.Text = "4";
+            this.B4.UseVisualStyleBackColor = true;
+            // 
+            // B5
+            // 
+            this.B5.Location = new System.Drawing.Point(94, 232);
+            this.B5.Name = "B5";
+            this.B5.Size = new System.Drawing.Size(75, 61);
+            this.B5.TabIndex = 6;
+            this.B5.Text = "5";
+            this.B5.UseVisualStyleBackColor = true;
+            // 
+            // B6
+            // 
+            this.B6.Location = new System.Drawing.Point(175, 232);
+            this.B6.Name = "B6";
+            this.B6.Size = new System.Drawing.Size(75, 61);
+            this.B6.TabIndex = 7;
+            this.B6.Text = "6";
+            this.B6.UseVisualStyleBackColor = true;
+            // 
+            // B7
+            // 
+            this.B7.Location = new System.Drawing.Point(13, 165);
+            this.B7.Name = "B7";
+            this.B7.Size = new System.Drawing.Size(75, 61);
+            this.B7.TabIndex = 8;
+            this.B7.Text = "7";
+            this.B7.UseVisualStyleBackColor = true;
+            // 
+            // B8
+            // 
+            this.B8.Location = new System.Drawing.Point(94, 165);
+            this.B8.Name = "B8";
+            this.B8.Size = new System.Drawing.Size(75, 61);
+            this.B8.TabIndex = 9;
+            this.B8.Text = "8";
+            this.B8.UseVisualStyleBackColor = true;
+            // 
+            // B9
+            // 
+            this.B9.Location = new System.Drawing.Point(175, 165);
+            this.B9.Name = "B9";
+            this.B9.Size = new System.Drawing.Size(75, 61);
+            this.B9.TabIndex = 10;
+            this.B9.Text = "9";
+            this.B9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 450);
+            this.Controls.Add(this.B9);
+            this.Controls.Add(this.B8);
+            this.Controls.Add(this.B7);
+            this.Controls.Add(this.B6);
+            this.Controls.Add(this.B5);
+            this.Controls.Add(this.B4);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.B0);
+            this.Controls.Add(this.outputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button B0;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B4;
+        private System.Windows.Forms.Button B5;
+        private System.Windows.Forms.Button B6;
+        private System.Windows.Forms.Button B7;
+        private System.Windows.Forms.Button B8;
+        private System.Windows.Forms.Button B9;
+    }
+}
+
